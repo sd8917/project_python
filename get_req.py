571612d2ea85b42d19b58fec83d59cc278291ca1
sudhanshuraj8917@gmail.com <sudhanshuraj8917@gmail.com>
2018-10-12 01:24:24 +0530

    get request

---
+++ get_req.py
@@ -0,0 +1,20 @@
+from flask import  Flask ,jsonify,request
+app = Flask (__name__)
+
+languages =[{'name':'Javascript'}, {'name':'Python'}, {'name':'Ruby'}]
+@app.route('/')
+def test():
+    return jsonify({'message' : 'It works!!'})
+
+@app.route('/lang',methods=['GET'])
+def returnAll():
+    return jsonify({'languages':languages})
+
+@app.route('/lang/<string:name>', methods= ['GET'])
+def returnOne(name):
+    langs = [language for language in languages if language ['name'] ==name]
+    return jsonify({'language':langs[0]})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
